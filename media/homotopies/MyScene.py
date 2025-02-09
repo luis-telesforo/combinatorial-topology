dae7d783f7fa8b73fd39eb527a73ece8bf337501
Luis Alberto <106775678+luis-telesforo@users.noreply.github.com>
2025-02-09 16:27:29 -0600

    Update .gitignore

---
+++ media/homotopies/MyScene.py
@@ -0,0 +1,22 @@
+from manim import Scene, Create, Uncreate, Text, Title
+
+ROSA = "#CC79A7"
+NARANJA = "#E69F00"
+AZUL = "#56B4E9"
+VERDOSO = "#0072B2"
+
+
+class MyScene(Scene):
+
+    def construct(self):
+        Text.set_default(font_size=20)
+
+    def create_title(self, title_text):
+        title = Title(title_text, include_underline=False)
+        self.play(Create(title))
+        self.wait(1.5)
+        self.play(Uncreate(title))
+
+    def add_description(self, description_text):
+        description = Title(description_text, font_size=30, include_underline=False)
+        self.play(Create(description))
---
+++ media/homotopies/main.py
@@ -0,0 +1,25 @@
+from manim import *
+
+from MyScene import MyScene, VERDOSO
+
+
+class HomotopyLine(MyScene):
+    def construct(self):
+        self.create_title("Ejemplo de homotopía")
+
+        ax = Axes(x_range=[-1, 1, 1], y_range=[-1, 1, 1], x_length=2, y_length=2)
+        line = ax.plot(lambda x: x, x_range=[0, 1])
+        line.set_z_index(2)
+        parabola = ax.plot(lambda x: x ** 2, x_range=[0, 1]).set_color(VERDOSO)
+        parabola.set_z_index(1)
+        description = r"$H(t,s) = (s, ts^{2}+(1-t)s)$ es una homotopía entre $f(s)=s$ y $g(s)=s^{2}$"
+        self.add_description(description)
+        self.play(Create(line.apply_function(lambda p: p * 3 - 1.5)))
+        self.play(Create(parabola.apply_function(lambda p: p * 3 - 1.5)))
+
+        def scaled_homotopy(x, y, z, t):
+            return x * 3 - 1.5, (t * (y ** 2) + (1 - t) * y) * 3 - 1.5, z
+
+        self.play(Homotopy(scaled_homotopy, line.apply_function(lambda p: (p + 1.5)/3), rate_func=linear, run_time=5))
+
+        self.wait()
