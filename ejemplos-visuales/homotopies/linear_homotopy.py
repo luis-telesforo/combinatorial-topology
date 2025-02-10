c4f6441c3d47c24080bbe2099d6dcc47fd148ef9
Luis Alberto <106775678+luis-telesforo@users.noreply.github.com>
2025-02-09 19:59:33 -0600

    Update linear_homotopy.py
    added another homotopy equivalence

--- ejemplos-visuales/homotopies/linear_homotopy.py
+++ ejemplos-visuales/homotopies/linear_homotopy.py
@@ -1,5 +1,6 @@
 import math
 
+from numpy import array, linalg
 from manim import *
 
 from basic_scene import MyScene, VERDOSO
@@ -40,3 +41,40 @@ class LinearHomotopy(MyScene):
             return x * SCALE - (SCALE/2), (t * (final_function(y)) + (1 - t) * initial_function(y)) * SCALE - (SCALE/2), z
 
         self.play(Homotopy(scaled_homotopy, function_at_0.apply_function(lambda p: (p + (SCALE / 2)) / SCALE), run_time=5))
+
+class NullHomotopy(MyScene):
+    def construct(self):
+        self.create_title("Ejemplo de homotopía")
+
+        description = r"$H(t,s) = (s, (1-t)s)$ es una homotopía entre $1_{S}$ y $g(s,t)=0$, donde $S$ es un convexo en $\mathbb{R}^{2}$."
+
+        self.add_description(description)
+
+        def null_homotopy(x, y, z, t):
+            return (1 - t) * x, (1 - t) * y, z
+
+        self.play(Homotopy(null_homotopy, Square(fill_opacity=1), run_time=5))
+
+        self.wait()
+
+class NoNullHomotopic(MyScene):
+    def construct(self):
+        self.create_title("Ejemplo de espacio no contráctil")
+
+        description = r"Un anillo es homotópicamente equivalente a un círculo. Y el círculo no es contráctil."
+
+        self.add_description(description)
+
+        ring = Annulus(1, 2, color=VERDOSO).set_z_index(1)
+
+        def null_homotopy(x, y, z, t):
+            first_coordinate = (1-t) * x + t*( x/ linalg.norm(array([x, y]), 2))
+            second_coordinate = (1-t) * y + t*( y/ linalg.norm(array([x, y]), 2))
+            return first_coordinate, second_coordinate, z
+        final_circle = Circle(radius=1, color= VERDOSO, stroke_width=2).set_z_index(2)
+        self.play(Homotopy(null_homotopy, ring, run_time=5), Add(final_circle, run_time=1))
+
+        self.wait()
+
+
+
